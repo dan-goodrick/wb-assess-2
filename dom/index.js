e4827844899dd848270d3d0f578669ea9b766c34
dan-goodrick <dannyjeee@yahoo.com>
2023-08-11 14:56:47 -0600

    End of assessment

--- dom/index.js
+++ dom/index.js
@@ -1,1 +1,31 @@
-// Replace this with your code
+document.querySelector("#close-form").addEventListener("click", (e) => {
+  let form = document.querySelector("form");
+  if (form.className == "form") {
+    form.className = "hide";
+    document.getElementById("close-form").innerText = "+";
+  } else {
+    form.className = "form";
+    document.getElementById("close-form").innerText = "X";
+  }
+});
+
+document.querySelector(".form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  let name = document.querySelector("#name").value;
+  let email = document.querySelector("#email").value;
+  console.log(name, email);
+  if (!name && !email) {
+    document.querySelector("#email").style.border = "2px solid red";
+    document.querySelector("#name").style.border = "2px solid red";
+    alert("You must enter a name and an email address to subscribe!");
+  } else if (!name) {
+    document.querySelector("#name").style.border = "2px solid red";
+    alert("Please enter a name to subscribe.");
+  } else if (!email) {
+    document.querySelector("#email").style.border = "2px solid red";
+    alert("Please enter an email address to subscribe.");
+  } else {
+    document.querySelector(".form").parentElement.innerText =
+      "Thank you for subscribing!";
+  }
+});
